78235f30a3cc07f76733d9144b38f2263696b20c
Lena <lena.dorohina12@gmail.com>
2026-06-10 02:41:55 +0300

    feat: init spring boot project and database schema

---
+++ src/main/java/kaleidostop/map/car_map/CarMapApplication.java
@@ -0,0 +1,13 @@
+package kaleidostop.map.car_map;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarMapApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarMapApplication.class, args);
+	}
+
+}
---
+++ src/test/java/kaleidostop/map/car_map/CarMapApplicationTests.java
@@ -0,0 +1,13 @@
+package kaleidostop.map.car_map;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarMapApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
